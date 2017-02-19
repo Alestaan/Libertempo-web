fb071bb35ecacacfe220f608c7570ccfc946b065
wouldsmina <wouldsmina@tuxfamily.org>
2017-02-19 23:01:50 +0100

    usage de la méthode canUserSaisieDemandePasse()

--- utilisateur/Fonctions.php
+++ utilisateur/Fonctions.php
@@ -223,7 +223,7 @@ class Fonctions
                     $datesDisabled[] = \App\Helpers\Formatter::dateIso2Fr($date);
                 }
             }
-            $startDate = ($_SESSION['config']['interdit_saisie_periode_date_passee']) ? 'd' : '';
+            $startDate = ($config->canUserSaisieDemandePasse()) ? 'd' : '';
 
             $datePickerOpts = [
                 'daysOfWeekDisabled' => $daysOfWeekDisabled,
@@ -1448,7 +1448,8 @@ class Fonctions
      */
     public static function getDatePickerStartDate()
     {
-        return ($_SESSION['config']['interdit_saisie_periode_date_passee']) ? 'd' : '';
+        $config = new \App\Libraries\Configuration();
+        return ($config->canUserSaisieDemandePasse()) ? 'd' : '';
     }
 
 
